af46f8a32162c335b559e0a44572b2367a94d71b
Christian Gram Kalhauge <christian@kalhauge.dk>
2013-11-11 00:24:21 +0100

    Minor change is valueset

--- fbml/valueset.py
+++ fbml/valueset.py
@@ -15,7 +15,8 @@ L = logging.getLogger(__name__)
 
 def analyse(method, arguments, valueset):
     """ Short analysis tool """
-    return method.evaluate(tuple(valueset.const(arg) for arg in arguments), valueset)
+    return method.evaluate(
+            tuple(valueset.const(arg) for arg in arguments), valueset)
 
 class ValueSet (object):
     """ ValueSet
