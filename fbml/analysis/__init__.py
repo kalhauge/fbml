b18c05491717435a593b472807c1903938e9db9f
Christian Gram Kalhauge <christian@kalhauge.dk>
2013-12-28 12:47:04 +0100

    PEP8 compliant

--- fbml/analysis/__init__.py
+++ fbml/analysis/__init__.py
@@ -16,7 +16,6 @@ import fbml
 
 
 class Evaluator(object):
-
     """
     The evaluator can evaluate a function using some basic
     methods.
@@ -46,9 +45,9 @@ class Evaluator(object):
             return new
 
     def bound_values(self, function, arguments):
-        """ Bound values of a  function """
-        bound_values = OrderedDict({k : self.valueset.const(v)
-                for k, v in function.constants.items()})
+        """ Bound values of a function """
+        bound_values = OrderedDict({k: self.valueset.const(v)
+                                    for k, v in function.constants.items()})
         bound_values.update(zip(function.arguments, arguments))
         return bound_values
 
@@ -63,7 +62,6 @@ class Evaluator(object):
         method_calls = zip(function.methods, repeat(bound_values))
         values = starmap(self.evaluate_method, method_calls)
         return reduce(valueset.merge, values, valueset.EXTREMUM)
-
 
     def evaluate_method(self, method, initial):
         """ Evalutates a method """
@@ -88,12 +86,16 @@ class Evaluator(object):
 
     def evaluate(self, function, args):
         """ Evaluates a function using real args """
-        return self.evaluate_function(function,
-                tuple(self.valueset.const(arg) for arg in args))
+        return self.evaluate_function(
+            function,
+            tuple(self.valueset.const(arg) for arg in args)
+        )
 
 
 class Modifier(object):
-
+    """
+    The modifier modifies a function.
+    """
     def __init__(self, valueset):
         self.valueset = valueset
 
@@ -109,29 +111,27 @@ class Modifier(object):
             def clean_node(node, sources):
                 """ Sources is the clean nodes """
                 func, arglist = self.depends[node]
-                all_results = lambda m : [self.evaluate_method(m, args)
-                         for args in arglist]
+                all_results = lambda m: [self.evaluate_method(m, args)
+                                         for args in arglist]
                 reached = [bool(
-                            reduce(
-                                self.valueset.merge,
-                                all_results(m),
-                                self.valueset.EXTREMUM
-                            ))
-                            for m in func.methods]
+                           reduce(
+                               self.valueset.merge,
+                               all_results(m),
+                               self.valueset.EXTREMUM
+                           ))
+                           for m in func.methods]
 
                 return fbml.node(model.SubFunction(func, reached), sources)
 
-
-            initial_map = {x:x for x in self.free_values(function) }
+            initial_map = {x: x for x in self.free_values(function)}
             valid_methods.append(
-                    model.Method(
-                        method.guard.visit(clean_node, initial_map ),
-                        method.statment.visit(clean_node, initial_map)
-                        ))
+                model.Method(
+                    method.guard.visit(clean_node, initial_map),
+                    method.statment.visit(clean_node, initial_map)
+                ))
 
         return function.define(function.constants, valid_methods)
 
 
 class FunctionNotValid(Exception):
     """ Method is not valid """
-
