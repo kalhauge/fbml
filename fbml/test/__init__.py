aa3e4cc15dd3f03b9a9c41606605095a37d86c3c
Christian Gram Kalhauge <christian@kalhauge.dk>
2014-03-16 23:54:30 -0700

    Added silly commentaries

--- fbml/test/__init__.py
+++ fbml/test/__init__.py
@@ -8,6 +8,10 @@ from fbml.model import Method, Function
 from fbml import buildin
 from fbml import node, statement, renode
 
+"""
+def incr(number):
+    number + 1
+"""
 INCR = statement(
     {'value': 1},
     node(buildin.add, {
@@ -32,55 +36,99 @@ MUL_IF_LESS = Function(
     'mul_if_less'
 )
 
+"""
+def add_to_all(numbers, incr):
+    generate list from [] using number in numbers:
+        append(list, (number + incr))
+
+"""
 ADD_TO_ALL = statement(
     {'empty' : tuple()},
     renode(
         statement(
             node(buildin.append, {
-                'list': node('reductor'),
+                'list': node('list'),
                 'elem': node(buildin.add, {
                     'a': node('number'),
                     'b': node('incr')
                 })
             })
         ),
-        ('reductor', node('empty')), # Reductor
+        ('list', node('empty')), # Reductor
         {'number': node('numbers')}, # Maps
         {'incr': node('incr')} #Variables
     ),
     'add_to_all'
 )
 
+"""
+# All compiles to the same thing:
+
+def sum_vectors(avector, bvector):
+    generate list from [] using a in avector, b in bvector:
+        append(list, (a + b))
+
+def sum_vectors(avector, bvector):
+    [ a + b | a in avector, b in bvector ]
+
+def sum_vector(avector, bvector):
+    avector + bvector
+
+"""
 SUM_VECTORS = statement(
     {'empty' : tuple()},
     renode(
         statement(
             node(buildin.append, {
-                'list': node('reductor'),
+                'list': node('list'),
                 'elem': node(buildin.add, {
                     'a': node('a'), 'b': node('b')
                 })
             })
         ),
-        ('reductor', node('empty')), # Reductor
+        ('list', node('empty')), # Reductor
         {'a': node('avector'), 'b': node('bvector')}, # Maps
         {} #Variables
     ),
     'sum_vectors'
 )
 
+"""
+def scalar_product(vector):
+    generate scalar from 1 using number in vector:
+        scalar * number
+"""
+
 SCALAR_PRODUCT = statement(
     {'initial' : 1},
     renode(
         statement(
             node(buildin.mul, {
-                'a': node('reductor'),
+                'a': node('scalar'),
                 'b': node('number')
             })
         ),
-        ('reductor', node('initial')), # Reductor
+        ('scalar', node('initial')), # Reductor
         {'number': node('vector')}, # Maps
         {} #Variables
     ),
     'sum_vectors'
 )
+
+"""
+def print_stuff(io, a, b, c):
+    using io do:
+        print(a)
+        print(b)
+        print(c)
+
+def print_stuff(io, a, b, c):
+    generate io form io using item in [a, b, c]:
+        io.print(item)
+
+def read_stuff(io):
+    using io do:
+        o = read()
+        write(result)
+    result = parse_int(o) + 10
+"""
